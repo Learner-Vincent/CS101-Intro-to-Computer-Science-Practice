d4e45c7e12962f90b184eb9220a39e32d44b697b
Vincent Li <learner_vincent@126.com>
2013-07-27 11:29:49 +0800

    Create lesson 4 #1 add to index.py

---
+++ lesson 4 #1 add to index.py
@@ -0,0 +1,14 @@
+ Define a procedure, add_to_index,
+# that takes 3 inputs:
+
+# - an index: [[<keyword>,[<url>,...]],...]
+# - a keyword: String
+# - a url: String
+
+# If the keyword is already
+# in the index, add the url
+# to the list of urls associated
+# with that keyword.
+
+# If the keyword is not in the index,
+# add an entry to the index: [keyword,[url]]

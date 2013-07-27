69ed7d958fad5bdbd5f682ae9ee41103d4c5571f
Vincent Li <learner_vincent@126.com>
2013-07-27 12:26:44 +0800

    Update

---
+++ lesson 1 #1 first programming quiz.py
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg936\cocoartf1187\cocoasubrtf370
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
+
+\f0\fs24 \cf0 # This is a Python comment. Lines that begin with a '#' are ignored by the\
+# Python interpreter. Comments are useful for documenting code or explaining\
+# quiz questions!\
+\
+# Write a Python program that prints out the number of minutes in seven weeks.\
+# Click the "Run" button below to try running your code and see the output,\
+# and click "Submit" to submit your answer.\
+\
+print 7 * 7 * 24 * 60}

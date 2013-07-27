0b3039dd2c18bd56f179a6037b576af99bcdfc99
Vincent Li <learner_vincent@126.com>
2013-07-27 09:03:05 +0800

    Update lesson 3 #14 lists of lists.py

--- lesson 3 #14 lists of lists.py
+++ lesson 3 #14 lists of lists.py
@@ -42,4 +42,36 @@ print total_enrollment(udacious_univs)
 
 print total_enrollment(usa_univs)
 #>>> (77285,3058581079L)
+
+
+# Another version
+udacious_univs = [['Udacity',90000,0]]
+
+usa_univs = [ ['California Institute of Technology',2175,37704],
+              ['Harvard',19627,39849],
+              ['Massachusetts Institute of Technology',10566,40732],
+              ['Princeton',7802,37000],
+              ['Rice',5879,35551],
+              ['Stanford',19535,40569],
+              ['Yale',11701,40500]  ]
+
+def total_enrollment(p):
+    numberStudents = 0
+    tuitionFees = 0
+    for univ, students, tuition in p:
+        numberStudents = numberStudents + students
+        tuitionFees = tuitionFees + students * tuition
+    return numberStudents, tuitionFees
+
+
+print total_enrollment(udacious_univs)
+#>>> (90000,0)
+
+# The L is automatically added by Python to indicate a long
+# number. If you are trying the question in an outside 
+# interpreter you might not see it.
+
+print total_enrollment(usa_univs)
+#>>> (77285,3058581079L)
     
+

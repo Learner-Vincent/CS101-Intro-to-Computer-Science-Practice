cadd5c67508f3fcb18ae7c1474fd8e68b9eabd96
Vincent Li <learner_vincent@126.com>
2013-08-19 12:45:54 +0800

    Create lesson 5 #1 empty hash table.py

---
+++ lesson 5 #1 empty hash table.py
@@ -0,0 +1,21 @@
+# Creating an Empty Hash Table
+# Define a procedure, make_hashtable,
+# that takes as input a number, nbuckets,
+# and returns an empty hash table with
+# nbuckets empty buckets.
+
+def make_hashtable(nbuckets):
+    table = [ ]
+    i = 0
+    while i < nbuckets:
+        table.append([ ])
+        i = i + 1
+    return table
+
+
+# Another version
+def make_hashtable(nbuckets):
+    table = [ ]
+    for nouse in range(0, nbuckets):
+        table.append([ ])
+    return table
